2f76ff78a4cc0defd212e9329d70975ed07e2094
barnrang <barnrang@gmail.com>
2018-08-08 20:04:46 +0900

    add database

--- index.js
+++ index.js
@@ -2,14 +2,21 @@ const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const { Pool } = require('pg');
 
-express()
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
+var app = express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
-  .get('/times', (req, res) => {
+
+app.get('/', (req, res) => res.render('pages/index'))
+app.get('/cool', (req, res) => res.send(cool()))
+app.get('/times', (req, res) => {
     let result = ''
     const times = process.env.TIMES || 5
     for (i = 0; i < times; i++) {
@@ -17,4 +24,17 @@ express()
     }
     res.send(result)
   })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+app.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect()
+    const result = await client.query('SELECT * FROM test_table');
+    res.render('pages/db', result);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+});
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
