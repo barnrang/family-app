15d8987eb7d79af9bbe51463baeae42429dd4cc2
barnrang <>
2018-08-10 01:44:45 +0900

    add options

--- public/scripts/test.js
+++ public/scripts/test.js
@@ -2,16 +2,38 @@ String.prototype.isEmpty = function() {
     return (this.length === 0 || !this.trim());
 };
 
+Vue.component('people-list', {
+  props: ['peopleList'],
+  template: '<select class=\"people-list\">\
+    <option \
+    v-for=\"person in peopleList\"\
+    value=\"person\">\
+    {{person}}</option>\
+    </select>',
+  computed: {
+    test: function () {
+      console.log(this.peopleList);
+    }
+  },
+  created() {
+    //console.log(this.peopleList);
+  }
+})
 
 Vue.component('todo-item', {
-  props:['todo'],
+  props:['todo', 'peopleList'],
   template: '<li class=\'todo-item\'>{{todo.name}}\
-   <span :class=\"classChoose\" @click=\"updateStatus\" ></span>\
+   <span :class=\"classChoose\" @click=\"updateStatus\" >{{moneyChoose}}</span>\
+   <people-list v-bind:people-list=\"peopleList\"></people-list>\
   </li>',
   computed: {
     classChoose: function () {
       if(this.todo.done) return 'todo-status done';
       else return 'todo-status undone'
+    },
+    moneyChoose: function () {
+      if(this.todo.money == -1) return '';
+      else return this.todo.money;
     }
   },
   methods: {
@@ -19,6 +41,9 @@ Vue.component('todo-item', {
       this.$emit('update-status', this.todo)
       console.log('click activate')
     }
+  },
+  created() {
+    //console.log(this.peopleList)
   }
 })
 
@@ -28,11 +53,12 @@ $(document).ready(() => {
     el: '#todolist',
     data: {
       jobs: [
-        {id:0, name: "test1", done: false},
-        {id:1, name: "test2", done: true}
+        {id:0, name: "test1", money:-1, done: false},
+        {id:1, name: "test2", money:100, done: true}
       ],
       newName: '',
       max_id: 1,
+      peopleList: ['John', 'Pat']
     },
     methods: {
       submit: function () {
