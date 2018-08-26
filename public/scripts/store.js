30e239b1defdd538b0df75100d6e51afc4fdebd1
NAPAT <barnrang@gmail.com>
2018-08-26 13:21:34 +0900

    add todo

--- public/scripts/store.js
+++ public/scripts/store.js
@@ -13,10 +13,12 @@ const store = new Vuex.Store({
     state: {
         jobs: [
             {id:0, name: "test1", money:0, done: false, acted: ['Pat'], resp: ['David']},
-            {id:1, name: "test2", money:100, done: true, acted: ['Pat'], resp: ['David']}
+            {id:1, name: "test2", money:100, done: false, acted: ['Pat'], resp: ['David']}
         ],
         max_id: 1,
-        peopleList: [{id:0, name:'John'}, {id:1, name:'Pat'}, {id:2, name:'David'}]
+        peopleList: [{id:0, name:'John'}, {id:1, name:'Pat'}, {id:2, name:'David'}],
+        payHistory: [],
+        currentPayStatus: [[0]],
     },
 
     mutations: {
@@ -30,6 +32,15 @@ const store = new Vuex.Store({
         },
         addElement(state, data) {
             state.jobs.push(data);
+        },
+        updateCurrentPayStatus(state, data){
+            //Update payment status (if done manually)
+        },
+        addPayHistory(state, data) {
+            //add payment history when user finish pay job
+        },
+        deletePayHistory(state, data) {
+            //delete payment history when user undo the complete pay job
         }
     }
 })
